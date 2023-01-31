5104245c20d41a94098fb1027170c644fe0259d0
marat2010 <marat.s.73@mail.ru>
2023-01-31 05:45:58 +0300

    Added docstring

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 """
-Краткое описание игры Крестики-нолики.
-Начинает игрок "Х". После игрок "О", и т.д. Чей ход показывает программа.
+        Краткое описание игры Крестики-Нолики.
+В начале вводятся входные параметры: цвета для игроков и необходимость отображения номеров клеток.
+Далее начинает игрок "Х". После игрок "О", и т.д. Чей ход показывает программа.
 Игрок может ввести одну цифру, тогда это номер клетки от 1 до 9, либо координаты из двух цифр от 1 до 3.
 
-free_cells: основной список, куда записываются ходы игроков "X" и "O" под индексом номера клетки.
-            Индекс 0 - клетка № 1.
+free_cells: основной список, куда записываются ходы игроков "X" и "O" под индексом номера клетки. Индекс 0 - клетка № 1.
         В начале игры в список помещаются номера клеток [1,2,3,..,7,8,9], после хода игрока "Х"
         например в клетку "2", список будет [1,"X",3,..,7,8,9] и т.д. Цифры будут переписаны "Х" и "О".
+        В случае выбора игры без отображения номеров клеток (параметр 'show_cells'), вместо цифр ставятся пробел (" ").
 Отображение игрового поля идет по списку free_cells, в зависимости от индекса.
-Отображение игроков "Х" и "О" на поле по цветам жирным шрифтом.
 Отображение номера клетки блеклым светлым цветом.
+Отображение игроков "Х" и "О" на поле жирным шрифтом, по цветам в зависимости от входных параметров "options",
+полученных в начале программы (Пример: {'color_X': 5, 'color_O': 6, 'show_cells': False, 'cells_wins': {0, 1, 2}}).
+Параметр 'cells_wins': {0, 1, 2} - отображает список "победных" клеток, которым назначается стиль "Мерцание".
 
 Некоторые места в программе можно было сделать проще.
 Например, использование генератора gen_row_cells, который просто последовательно выдает по 3 значения
  из списка free_cells. Проще было сделать копию этого списка, и удалять из него по три элемента,
  перезаписывая список:
                         new_free_cells = free_cells.copy()
-             В цикле:   row = new_free_cells[:3]
+          В теле цикла: row = new_free_cells[:3]
                         s = s.format(*row)
                         new_free_cells = new_free_cells[3:]
 
 Эти упрощения не делались намеренно мной, хотелось применить полученные навыки из материалов обучения,
-важных таких как генераторы, декораторы, all, any, map, list comprehension, Тернарный оператор, ... )
-
+важных таких как генераторы, декораторы, all, any, map, list comprehension, тернарный оператор, ... )
 """
 
 
-def gen_row_cells(free_cells):  # генератор выдачи по три ячейки строки
+def gen_row_cells(free_cells: list) -> list:
+    """
+    Генератор выдачи по три ячейки для игрового поля из основного списка free_cells.
+    :param free_cells: list.
+        Основной список ходов игроков. Например: [1,'O','X',4,'X',6,'O','X',9]
+        или [' ','O','X',' ','X',' ','O','X',' '] если вывод поля, без отображения номеров клеток.
+    :return yield row: list.
+        Отдача по (для) одной строки. При первом обращении отдаст:
+            [1,'O','X'] или [' ','O','X'], потом:
+            [4,'X',6,] или [' ','X',' '], потом:
+            ['O','X',9] или ['O','X',' ']
+    """
     cells = free_cells.copy()
     while True:
         row = cells[:3]
@@ -36,14 +49,14 @@ def gen_row_cells(free_cells):  # генератор выдачи по три я
             break
 
 
-def change_char(char: str, options: dict):
+def change_char(char: str, ch_options: dict) -> str:
     """
     Изменение цвета и атрибута символа или текста.
-    :param char: str
+    :param char: str.
         Символ или строка, у которой надо поменять цвет, или стиль.
-    :param options: dict
-        'color': int
-            Цвет, на который надо поменять.
+    :param ch_options: dict:
+        'color': int.
+            Цвет, на который надо поменять. Формат вставки "\033[{код цвета или стиля}m"
                 Чёрный 30 (Фон 40)
                 Красный 31 (Фон 41)
                 Зелёный 32 (Фон 42)
@@ -62,26 +75,30 @@ def change_char(char: str, options: dict):
                 5 Редкое мигание
                 6 Частое мигание
                 7 Смена цвета фона с цветом текста.
-    :return: char: str
-        Символ или строка в новом цвете и стиле
+    :return: char: str.
+        Символ или строка в новом цвете и стиле. Например "\033[1m\033[34mМОЙ_ТЕКСТ\033[0m" - для текста "МОЙ_ТЕКСТ"
     """
-
-    char = f"\033[{options['color']+30}m{char}\033[0m"  # Добавляем + 30, код цвета начинается с 30
-    for s in options['style']:
+    char = f"\033[{ch_options['color'] + 30}m{char}\033[0m"  # Добавляем + 30, код цвета начинается с 30
+    for s in ch_options['style']:  # Добавляем стили
         char = f"\033[{s}m" + char
     return char
 
 
 def color_free_cells(func):
     """
-    Изменение цветов значков в игровом поле
+    Декоратор. Изменение цвета значков для игрового поля.
     :param func:
-    :return:
+    Принимает из функции (args[0]) основной список значений игрового поля вида [1,'O','X',4,'X',6,'O','X',9].
+    Из kwargs: 'color_X' - цвет для значка "Х", 'color_O' - цвет для значка "О".
+    Для цифр игрового поля, т.е. пустых клеток (номер клетки) задается Блёклый белый цвет.
+    При победе одного из игрока в kwargs появляется ключ 'cells_wins', где лежат номера "победных" клеток.
+    "Победным" клеткам добавляется мерцание.
+    :return: func: с измененным "цветным" списком значений игрового поля.
     """
     def wrapper(*args, **kwargs):
-        cells = args[1]  # Чтение списка значений клеток
+        cells = args[0]  # Чтение списка значений клеток
         new_cells = []  # Новый список цветных значений
-        print(f" kwargs=== {kwargs}")
+        # print(f" Параметры 'options' (kwargs): {kwargs}")
 
         for ch in cells:  # Для каждой клетки меняем цвет и стиль
             if ch == 'X':
@@ -91,34 +108,43 @@ def color_free_cells(func):
                 color = kwargs['color_O']
                 ch = change_char(ch, {'color': color, 'style': [1]})
             else:
-                ch = change_char(ch, {'color': 7, 'style': [2, 3]})
+                ch = change_char(ch, {'color': 7, 'style': [2, 3]})  # Для пустых клеток.
             new_cells.append(ch)
 
         if kwargs.get('cells_wins'):  # Если кто-то выиграл.
             cells_wins = kwargs.get('cells_wins')
             for i in cells_wins:  # Для каждой такой клетки
                 ch = new_cells[i]
-                ch = change_char(ch, {'color': 0, 'style': [5]})  # Делаем мерцание
+                ch = change_char(ch, {'color': 0, 'style': [5]})  # Добавляем мерцание
                 new_cells[i] = ch
 
-        args = args[0], new_cells
-        return func(*args, **kwargs)
+        args = new_cells
+        return func(args, **kwargs)
     return wrapper
 
 
 @color_free_cells
-def show_fields(user, free_cells, **kwargs):
+def show_fields(free_cells: list, **kwargs: dict) -> None:
+    """
+    Отображение игрового поля
+    :param free_cells: Список значений полей
+    :param kwargs: Параметры "options", полученных в начале программы.
+     (Пример: {'color_X': 5, 'color_O': 6, 'show_cells': False, 'cells_wins': {0, 1, 2}})
+     Параметр 'cells_wins': {0, 1, 2} формируется в момент выигрыша.
+     kwargs - в самой функции не используется, необходим для декоратора, для придания цвета значков "Х" "О".
+    :return:
+    """
     print('\n ↓x\y→', end=' ')
     print('1' + ' ' * 3 + '2' + ' ' * 3 + '3')
 
     top_1 = (' ' * 5 + '┌───┬───┬───┐\n' + '  1  ')
-    middle_1 = ('│' + ' {} ') * 3
+    middle_1 = ('│' + ' {} ') * 3  # В эти строки будем подставлять "Х", "О" или номер клетки, или пустоту.
     bottom_1 = ('│\n' + ' ' * 5 + '├───┼───┼───┤\n' + '  2  ')
 
-    middle_2 = ('│' + ' {} ') * 3
+    middle_2 = ('│' + ' {} ') * 3  # В эти строки будем подставлять "Х", "О" или номер клетки, или пустоту.
     bottom_2 = ('│\n' + ' ' * 5 + '├───┼───┼───┤\n' + '  3  ')
 
-    middle_3 = ('│' + ' {} ') * 3
+    middle_3 = ('│' + ' {} ') * 3  # В эти строки будем подставлять "Х", "О" или номер клетки, или пустоту.
     bottom_3 = ('│\n' + ' ' * 5 + '└───┴───┴───┘\n')
 
     lst = [top_1, middle_1, bottom_1, middle_2, bottom_2, middle_3, bottom_3]
@@ -126,8 +152,8 @@ def show_fields(user, free_cells, **kwargs):
     gen_row = iter(gen_row_cells(free_cells))  # Генератор 3-х значений из списка [[1,2,3], [4.. ]
     # new_free_cells = free_cells.copy()  # В случае не использования генератора gen_row
 
-    for i, s in enumerate(lst):
-        if i % 2:  # Строки куда необходимо подставить значения из списка free_cells
+    for string_id, s in enumerate(lst):
+        if string_id % 2:  # Строки (нечетные) куда необходимо подставить значения из списка free_cells
             s = s.format(*next(gen_row))
             # В случае не использования генератора gen_row:
             # row = new_free_cells[:3]
@@ -136,8 +162,11 @@ def show_fields(user, free_cells, **kwargs):
         print(s, end='')
 
 
-def cell_calculation(xy: list):
-    """Вычисление клетки по координатом (x,y)"""
+def cell_calculation(xy: list) -> int:
+    """Вычисление клетки по координатом (x,y)
+    :param xy: list. Координаты из двух цифр
+    :return: cell: int. Номер клетки
+    """
     cell = xy[1]
     if xy[0] == 2:
         cell = xy[1] + 3
@@ -146,10 +175,16 @@ def cell_calculation(xy: list):
     return cell
 
 
-def users_input(user: str, fr_cell: list):
+def users_input(user: str, fr_cell: list) -> int:
+    """
+    Ввод данных пользователем номера клетки или координаты клетки.
+    :param user: "Х" или "О"
+    :param fr_cell: Основной список клеток, где помечены ходы пользователя и пустые клетки.
+    :return: cell: int. Номер клетки (1-9), куда сходил игрок.
+    """
     while True:
         cell = 0
-        place = input(f"\tХодит: {user}\n\nВведите номер клетки (1-9) или координаты (x y): ").split()
+        place = input(f"\tХодит: {user}\n\nВведите номер клетки (1-9) или координаты 'x' 'y': ").split()
 
         if not all(p.isdigit() for p in place):
             print("\nНе число!!!\n")
@@ -170,7 +205,6 @@ def users_input(user: str, fr_cell: list):
             print("\nВведите одно или два числа!!!\n")
             continue
 
-        # if cell not in fr_cell:
         if fr_cell[cell-1] == 'X' or fr_cell[cell-1] == 'O':
             print("\nКлетка занята!!!\n")
             continue
@@ -178,26 +212,40 @@ def users_input(user: str, fr_cell: list):
     return cell
 
 
-def win_position(user: str, cells: list, **kwargs):
-    """ Проверка комбинаций на выигрыш """
+def win_position(user: str, cells: list, **kwargs) -> bool:
+    """
+     Проверка комбинаций на выигрыш.
+    :param user: str. Игрок "Х" или "О"
+    :param cells: list. Основной список клеток, где помечены ходы пользователя и пустые клетки.
+    :param kwargs: 'cells_wins'. Набор "победных" клеток.
+    :return: bool. Есть выигрыш или нет.
+    """
     positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     user_steps = set([i for i, x in enumerate(cells) if x == user])  # Ходы игрока "X" или "O"
 
     for p in positions:
-        if len(user_steps.intersection(set(p))) == 3:
-            kwargs['cells_wins'] = user_steps.intersection(set(p))
-            show_fields(user, cells, **kwargs)
+        match_set = user_steps.intersection(set(p))  # Набор совпадений ходов и выигрышных позиций
+        if len(match_set) == 3:
+            kwargs['cells_wins'] = match_set  # передаем выигрышный набор в прорисовку игрового поля
+            show_fields(cells, **kwargs)
             return True
     return False
 
 
 def start(**kwargs):
-    # print('===kwargs====', kwargs['show_cells'], kwargs)
+    """
+    Основная логика программы.
+    :param kwargs: Входные параметры для игры:
+        'color_X': Цвет игрока "Х",
+        'color_O': Цвет игрока "О",
+        'show_cells': Показывать или нет номера клеток на игровом поле})
+    :return:
+    """
     free_cells = (list(range(1, 10)) if kwargs['show_cells'] else [' ']*9)  # основной список игрового поля
     while True:
         step_count = free_cells.count('X') + free_cells.count('O')  # Кол-во ходов
         user = 'O' if step_count % 2 else 'X'  # Определяем пользователя, чей ход
-        show_fields(user, free_cells, **kwargs)  # Рисуем игровое поле
+        show_fields(free_cells, **kwargs)  # Рисуем игровое поле
 
         if step_count < 9:
             cell = users_input(user, free_cells)
@@ -213,12 +261,14 @@ def start(**kwargs):
 
 
 if __name__ == '__main__':
+    """  Игра крестики нолики. Ввод параметров для игры. """
+
     colors = [" 1. Чёрный", " 2. Красный", " 3. Зелёный", " 4. Жёлтый", " 5. Синий (по умолчанию 'X')",
               " 6. Фиолетовый", " 7. Бирюзовый (по умолчанию 'O')", " 8. Белый"]
 
     print("\n Чтобы оставить по умолчанию нажмите Enter\n")
     for i, c in enumerate(colors):
-        c = change_char(c, {'color': i, 'style': []})
+        c = change_char(c, {'color': i, 'style': []})  # Изменение цвета текста (Для черного цвета код 30)
         print(c)
 
     color_X = input(f"\n Выберите цвет игрока 'X': ")
@@ -227,12 +277,12 @@ if __name__ == '__main__':
     color_X = (int(color_X)-1 if color_X.isdigit() and (0 < int(color_X) <= 8) else 4)
     color_O = (int(color_O)-1 if color_O.isdigit() and (0 < int(color_O) <= 8) else 6)
 
-    show_cells = input(f"\n Показывать номера клеток? (по умолчанию ДА, для изменения введите что угодно)")
+    show_cells = input(f"\n Показывать номера клеток? (по умолчанию ДА, для изменения введите любой символ) ")
     show_cells = (False if show_cells else True)
 
-    config = {'color_X': color_X, 'color_O': color_O, 'show_cells': show_cells}
+    options = {'color_X': color_X, 'color_O': color_O, 'show_cells': show_cells}
 
-    start(**config)
+    start(**options)
 
 
 
@@ -240,6 +290,10 @@ if __name__ == '__main__':
 # kwargs=== {'color_X': 4, 'color_O': 6, 'show_cells': '', 'cells_wins': {8, 2, 5}}
 # kwargs - основной словарь, где хранится конфигурация игры и выигрышные ячейки
 # ===============================================================
+# if cell not in fr_cell:
+    # print('===kwargs====', kwargs['show_cells'], kwargs)
+# show_fields(user, free_cells, **kwargs)  # Рисуем игровое поле
+
 # ----------------------------------------------------------
 # ----------------------------------------------------------
 # if user == 'X':
